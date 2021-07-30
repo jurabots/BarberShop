986d473c9ad847bb47388e84ecbeee7e4d1a63ec
jura.bots@gmail.com <jura.bots@gmail.com>
2021-07-30 16:56:17 +0300

    Add DB price and service element and check for null and inappropriate quantity

---
+++ BarberShop/Controllers/PriceController.cs
@@ -0,0 +1,38 @@
+﻿using BarberShop.Context;
+using BarberShop.InitializationDB;
+using BarberShop.Models.ServicesAndPrice;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberShop.Controllers
+{
+    public class PriceController : Controller
+    {
+        public PriceController(BarberShopDB context)
+        {
+            Context = context;
+
+
+        }
+        public BarberShopDB Context { get; }
+
+        public IActionResult ShowPriceAndService()
+        {
+            if (Context.Service.Count() == 0 || Context.Service.Count() < 6)
+            {
+                var service = Context.Service
+                    .Where(x => x.Id >= 1).ToList();
+                Context.Service.RemoveRange(service);
+                SingletonInitServiceAndPrice init = SingletonInitServiceAndPrice.NewInstance();
+                Context.Service.AddRange(init.AddServicePrice());
+                Context.SaveChanges();
+            }
+            return View();
+        }
+    }
+  
+
+}
---
+++ BarberShop/InitializationDB/SingletonInitServiceAndPrice.cs
@@ -0,0 +1,41 @@
+﻿using BarberShop.Context;
+using BarberShop.Models.ServicesAndPrice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberShop.InitializationDB
+{
+    public class SingletonInitServiceAndPrice
+    {
+        private static SingletonInitServiceAndPrice init = null;
+        protected SingletonInitServiceAndPrice()
+        {
+           
+        }
+
+        public static SingletonInitServiceAndPrice NewInstance()
+        {
+            if (init == null)
+            {
+                init = new SingletonInitServiceAndPrice();
+            }
+            return init;
+        }
+
+        public List<Service> AddServicePrice()
+        {
+            List<Service> services = new List<Service>()
+            {
+                new Service { OurService = "Мужская стрижка", Price = 150 },
+                new Service { OurService = "Стрижка бороды",Price = 100 },
+                new Service { OurService = "Стрижка усов",Price = 80},
+                new Service { OurService = "Стрижка под насадку", Price = 110},
+                new Service { OurService = "Детская стрижка(до 11 лет)",Price = 100},
+                new Service { OurService = "Бритье опасной бритвой", Price = 120}
+            };           
+            return services;
+        }
+    }
+}
